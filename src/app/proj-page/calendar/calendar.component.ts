0e3274253dba6eaa85a1cf11a07d283a500904a0
Joao Lucas Cruz <jaotuta@hotmail.com>
2023-04-17 22:15:59 -0300

    troca de mes calendário

--- src/app/proj-page/calendar/calendar.component.ts
+++ src/app/proj-page/calendar/calendar.component.ts
@@ -3,17 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent {
-
   //dayName = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado")
-  monName = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+  monName = new Array(
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  );
   diasDoMes = new Array();
-  now = new Date
+  now = new Date();
   mes = this.now.getMonth();
   ano = this.now.getFullYear();
-  qtdDiasNoMes = this.diasNoMes(this.mes, this.ano)
+  qtdDiasNoMes = this.diasNoMes(this.mes, this.ano);
   diaDeInicio = this.retornaPrimeiroDiaDoMes(this.mes, this.ano);
   firstLoad = 0;
   diaSelecionado: undefined;
@@ -22,53 +34,82 @@ export class CalendarComponent {
     var data = new Date(ano, mes + 1, 0);
     return data.getDate();
   }
-
+  prevMonth() {
+    if (!(this.mes < 1)) {
+      console.log(this.mes);
+      this.mes = this.mes - 1;
+      this.qtdDiasNoMes = this.diasNoMes(this.mes, this.ano);
+      console.log(this.mes);
+      this.ngOnInit();
+    }
+  }
+  nextMonth() {
+    if (!(this.mes > 10)) {
+      console.log(this.mes);
+      this.mes = this.mes + 1;
+      this.qtdDiasNoMes = this.diasNoMes(this.mes, this.ano);
+      console.log(this.mes);
+      this.ngOnInit();
+    }
+  }
   retornaPrimeiroDiaDoMes(mes: number, ano: number) {
     var data = new Date(ano, mes, 1);
-    return data.getDay()
+    return data.getDay();
   }
 
   getDiaSelecionado($event: any) {
     this.diaSelecionado = $event.target.innerHTML;
-    console.log( this.diaSelecionado);
+    console.log(this.diaSelecionado);
   }
 
   ngOnInit() {
-    for( let i = 1; i <= this.qtdDiasNoMes; i++) {
+    this.diasDoMes = [];
+    for (let i = 1; i <= this.qtdDiasNoMes; i++) {
       this.diasDoMes.push(i);
     }
-
   }
 
   ngDoCheck() {
-    if(this.firstLoad < 3){
-      console.log('entrou')
-      const primeiroDia = document.querySelector(".dia-numerico");
-      const hoje = document.querySelectorAll(".conteudo");
-      hoje.forEach(item => {
-        if (parseInt(item.innerHTML) == this.now.getDate()) {
-          item.classList.add("dia-atual")
-        }
-      })
+    console.log('entrou');
+    const primeiroDia = document.querySelector('.dia-numerico');
+    const hoje = document.querySelectorAll('.conteudo');
+    hoje.forEach((item) => {
+      if (
+        parseInt(item.innerHTML) == this.now.getDate() &&
+        this.mes == this.now.getMonth()
+      ) {
+        console.log(this.mes == this.now.getMonth());
+        item.classList.add('dia-atual');
+      } else {
+        item.classList.remove('dia-atual');
+      }
+    });
+    if (this.firstLoad < 3) {
       switch (this.diaDeInicio) {
-        case 0: primeiroDia?.classList.add("domingo");
+        case 0:
+          primeiroDia?.classList.add('domingo');
           break;
-        case 1: primeiroDia?.classList.add("segunda");
+        case 1:
+          primeiroDia?.classList.add('segunda');
           break;
-        case 2: primeiroDia?.classList.add("terca");
+        case 2:
+          primeiroDia?.classList.add('terca');
           break;
-        case 3: primeiroDia?.classList.add("quarta");
+        case 3:
+          primeiroDia?.classList.add('quarta');
           break;
-        case 4: primeiroDia?.classList.add("quinta");
+        case 4:
+          primeiroDia?.classList.add('quinta');
           break;
-        case 5: primeiroDia?.classList.add("sexta");
+        case 5:
+          primeiroDia?.classList.add('sexta');
           break;
-        case 6: primeiroDia?.classList.add("sabado");
+        case 6:
+          primeiroDia?.classList.add('sabado');
           break;
       }
     }
 
-    this.firstLoad ++;
+    this.firstLoad++;
   }
-
 }
